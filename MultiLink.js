1a4bfc83425ba478f5412205d197dabe52825f57
ilhamerlianx <36921647+ilhamerlianx@users.noreply.github.com>
2018-07-26 15:00:14 +0700

    Update MultiLink.js

--- MultiLink.js
+++ MultiLink.js
@@ -19,10 +19,9 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*2)+ 1);
+var x=Math.floor((Math.random()*1)+ 1);
 var xxx=null;
-if(x=="1"){xxx="cheaper-car-insurance-tips.html"}
-if(x=="2"){xxx="cheap-car-insurance-global.html"}a_to_ve[i].href="https://mazencryptx.blogspot.com/2018/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="safeads.html"}a_to_ve[i].href="http://www.healthaccworld.xyz/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
